f3defbb56f21da15518dbc52da61aab5949d8362
devnokiyo <devnokiyo@gmail.com>
2021-04-06 02:11:08 +0900

    ログローテーション導入

--- app/common/config/logging/zap.go
+++ app/common/config/logging/zap.go
@@ -1,23 +1,25 @@
 package logging
 
 import (
+	"io"
+	"time"
+
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func Setup(options ...zap.Option) (*zap.Logger, error) {
-	return zap.Config{
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
-		Development: false,
-		Sampling: &zap.SamplingConfig{
-			Initial:    100,
-			Thereafter: 100,
-		},
-		Encoding:         "json",
-		EncoderConfig:    encoderConfig(),
-		OutputPaths:      []string{"stderr", "/var/log/app/access.log"},
-		ErrorOutputPaths: []string{"stderr"},
-	}.Build(options...)
+func Setup(options ...zap.Option) *zap.Logger {
+	hook1 := getWriter("/var/log/app/access.log")
+	w := zapcore.AddSync(hook1)
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig()),
+		w,
+		zap.InfoLevel,
+	)
+	logger := zap.New(core)
+
+	return logger
 }
 
 func encoderConfig() zapcore.EncoderConfig {
@@ -35,4 +37,18 @@ func encoderConfig() zapcore.EncoderConfig {
 		EncodeDuration: zapcore.SecondsDurationEncoder,
 		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
+}
+
+func getWriter(filename string) io.Writer {
+	hook, err := rotatelogs.New(
+		filename+".%Y%m%d",
+		rotatelogs.WithLinkName(filename),
+		rotatelogs.WithMaxAge(time.Hour*24*7),
+		rotatelogs.WithRotationTime(time.Hour*24),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	return hook
 }
